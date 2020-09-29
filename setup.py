dccf2c8dc6e88654600a59715bc014eaa9f85589
Alican Toprak <tordox@gmail.com>
2020-09-29 22:03:14 +0200

    removed py34 from setup.py

--- setup.py
+++ setup.py
@@ -55,7 +55,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
